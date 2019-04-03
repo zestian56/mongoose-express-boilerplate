2ae0ad2ee35899fb2541ccd210a61de2676cf212
zestian56 <sebas-565@hotmail.com>
2019-04-02 20:23:29 -0500

    Add: User schema

--- src/common/models/user/user.model.js
+++ src/common/models/user/user.model.js
@@ -5,7 +5,49 @@ const userSchema = new Schema({
         type: String,
         required: true,
         unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    nationality: {
+        type: String
+    },
+    documentId: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    gender: {
+        type: String,
+        trim: true
+    },
+    phone: {
+        type: String,
+        trim: true
+    },
+    cellphone: {
+        type: String,
+        trim: true
+    },
+    address: {
+        type: String,
+        trim: true
+    },
+    eps: {
+        type: String
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
     }
+
 });
 
 userSchema.set('toObject', {
@@ -13,8 +55,8 @@ userSchema.set('toObject', {
 });
 userSchema.method("toGraph", function toGraph() {
     return JSON.parse(JSON.stringify(this));
-  });
-  
+});
+
 const User = model('User', userSchema);
 
 export default User;
